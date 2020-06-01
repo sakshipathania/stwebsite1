6807c0dfacfa58f6462c551e05372a70242daadb
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-06-01 13:33:27 +0530

    Update paypal_checkout.java

--- src/test/java/SetupClass/TestStep/paypal_checkout.java
+++ src/test/java/SetupClass/TestStep/paypal_checkout.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
 
 import SetupClass.Set;
 import cucumber.api.java.en.Given;
@@ -17,7 +18,8 @@ import cucumber.api.java.en.Then;
 public class paypal_checkout extends Set {
 	
 	WebDriverWait wait = new WebDriverWait(driver,10);
-
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+       
 	@Given("^user is already on Website Home Page pp$")
 	public void user_is_already_on_Website_Home_Page_pp() throws Throwable {
 		
@@ -240,7 +242,7 @@ public class paypal_checkout extends Set {
 		
 
 		 driver.findElement(By.xpath("//a[contains(.,'My Account')]")).click();
-		js.executeScript("arguments[0].scrollIntoView();",delete_account);
+		//js.executeScript("arguments[0].scrollIntoView();",delete_account);
 		 Thread.sleep(3000);
 		 
 		 WebElement delete_account = driver.findElement(By.xpath("//a[contains(text(),'Delete Account')]"));
