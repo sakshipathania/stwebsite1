47039f784ce03a0e4f19b41317741159aefca15c
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-11-06 17:37:13 +0530

    Update Google_new_user_sign_up.java

--- src/test/java/SetupClass/TestStep/Google_new_user_sign_up.java
+++ src/test/java/SetupClass/TestStep/Google_new_user_sign_up.java
@@ -32,10 +32,11 @@ public class Google_new_user_sign_up extends Set {
 		
 		try {
 			// click on sign in with google button on home page
-			//driver.findElement(By.xpath("//a[contains(text(),'Sign in with Google')]")).click();
+			driver.findElement(By.xpath("//a[contains(text(),'Sign in with Google')]")).click();
+			driver.findElement(By.xpath("//a[contains(text(),'Sign in with Google')]")).click();
 			//driver.findElement(By.className("btn btn-block btn-social btn-google social-btn")).click();
 			//Thread.sleep(1000);
-			driver.get("https://mail.google.com");
+			//driver.get("https://mail.google.com");
 			Thread.sleep(1000);
 			log.info("It's opening the website URL");
 		} 
