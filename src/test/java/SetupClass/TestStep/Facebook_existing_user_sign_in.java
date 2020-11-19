a4da666c6024b9da570ffc75592ebb072c66ef4c
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-11-19 13:35:29 +0530

    Update Facebook_existing_user_sign_in.java

--- src/test/java/SetupClass/TestStep/Facebook_existing_user_sign_in.java
+++ src/test/java/SetupClass/TestStep/Facebook_existing_user_sign_in.java
@@ -62,7 +62,10 @@ public class Facebook_existing_user_sign_in extends Set{
 		Thread.sleep(2000);
 
 	    fb_login_btn.click();
-		
+		Thread.sleep(4000);
+	WebElement privacy =wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#platformDialogForm > div._5lnf.uiOverlayFooter._5a8u > table > tbody > tr > td._51m-.prs.uiOverlayFooterMessage > table > tbody > tr > td._51m-.uiOverlayFooterButtons._51mw > button._42ft._4jy0.layerConfirm._51_n.autofocus._4jy5._4jy1.selected._51sy")));
+                Thread.sleep(3000);
+		privacy.click();
 	}
 
 	@Then("^user go to free ppts page old fb$")
