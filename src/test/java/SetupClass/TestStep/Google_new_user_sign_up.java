ced292173acf00f1b2eae16f296cb329e67cf424
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-02-23 16:03:23 +0530

    Update Google_new_user_sign_up.java

--- src/test/java/SetupClass/TestStep/Google_new_user_sign_up.java
+++ src/test/java/SetupClass/TestStep/Google_new_user_sign_up.java
@@ -134,7 +134,7 @@ public class Google_new_user_sign_up extends Set {
 		 Thread.sleep(3000);
 
 
-try {
+/*try {
 			WebElement iframe = driver.findElement(By.id("livechat-full-view"));
 			if(iframe.isDisplayed()) {
 				driver.switchTo().frame(iframe);   
@@ -170,7 +170,18 @@ try {
 		 continue_delete.click();
 		 Thread.sleep(3000);
 		
-	}
+	}*/
+		try {
+			WebElement logout = driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]"));
+			if (logout.isEnabled()) {
+				logout.click();
+				Thread.sleep(8000);
+				driver.navigate().refresh();
+				Thread.sleep(2000);
+			}
+		} catch (NoSuchElementException Ext) {
+
+		}
 
 
 
