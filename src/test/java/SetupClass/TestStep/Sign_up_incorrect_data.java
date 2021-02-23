0a7c30671414593edd4079d496d5c1a59f6e8289
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-02-23 16:05:08 +0530

    Update Sign_up_incorrect_data.java

--- src/test/java/SetupClass/TestStep/Sign_up_incorrect_data.java
+++ src/test/java/SetupClass/TestStep/Sign_up_incorrect_data.java
@@ -248,7 +248,7 @@ Thread.sleep(3000);
 		 driver.findElement(By.cssSelector("ul.header > li:nth-child(1) > a:nth-child(1)")).click();
 		 Thread.sleep(3000);
 		 
-try {
+/*try {
 			WebElement iframe = driver.findElement(By.id("livechat-full-view"));
 			if(iframe.isDisplayed()) {
 				driver.switchTo().frame(iframe);   
@@ -284,7 +284,18 @@ try {
 		js.executeScript("arguments[0].scrollIntoView();",continue_delete);
 		 continue_delete.click();
 		 Thread.sleep(3000);
-	}
+	}*/
+		try {
+			WebElement logout = driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]"));
+			if (logout.isEnabled()) {
+				logout.click();
+				Thread.sleep(8000);
+				driver.navigate().refresh();
+				Thread.sleep(2000);
+			}
+		} catch (NoSuchElementException Ext) {
+
+		}
 
 
 
