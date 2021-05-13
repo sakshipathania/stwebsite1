a46ab71328d554fceb97b60de9f167d71e4edec7
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-05-13 14:09:07 +0530

    Update Stripe_checkout.java

--- src/test/java/SetupClass/TestStep/Stripe_checkout.java
+++ src/test/java/SetupClass/TestStep/Stripe_checkout.java
@@ -292,7 +292,7 @@ public class Stripe_checkout extends Set{
 		WebElement delete_profile_coupon = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#flipModal > div > div > div.modal-footer.button_action > button.btn.btn-default.button_2")));
 		js.executeScript("arguments[0].scrollIntoView();",delete_profile_coupon);
 		delete_profile_coupon.click();
-		 Thread.sleep(3000);*/
+		 Thread.sleep(3000);
 		
 		/*try {
 			WebElement logout = driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]"));
